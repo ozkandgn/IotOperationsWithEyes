793c8221b3591646d6a67a4d69c6f88db01c49a2
engolos <yilmazengin007@gmail.com>
2019-03-25 17:10:39 +0300

    Add MQTT

---
+++ communicationPubSub/deneme.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt #import the client1
+import time
+############
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+########################################
+broker_address="Pi Ip Address"
+
+print("creating new instance")
+client = mqtt.Client("P1") #create new instance
+client.on_message=on_message #attach function to callback
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+client.loop_start() #start the loop
+print("esp8266","esp8266")
+client.subscribe("esp8266")
+print("esp8266","esp8266")
+client.publish("esp8266","AT+LEDO")
+time.sleep(4) # wait
+client.loop_stop() #stop the loop
