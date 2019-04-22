16182de8f2e27a3f71b84300aafba9a77d4da522
Ozan Şahin <ozansahin97@hotmail.com>
2019-04-23 00:39:01 +0300

    Made interface greater

--- Interface/interface_Opr.py
+++ Interface/interface_Opr.py
@@ -3,29 +3,47 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication,QDialog, QLabel
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from PyQt5.uic import loadUi
 from PyQt5 import *
+from television import Television
+from book import Book
 
-class denemem(QDialog):
+class MainUI(QDialog):
     def __init__(self):
-        super(denemem,self).__init__()
-        loadUi('interfaceV4.ui',self)
-        self.setWindowTitle('Tez Deneme UI')
+        super(MainUI,self).__init__()
+        loadUi('interface.ui',self)
+        self.setWindowTitle('MAIN PAGE UI')
+        self.setWindowIcon(QtGui.QIcon('Photos/eye.jpg'))
+        self.setStyleSheet("QDialog{background-image: url(Photos/room.jpg); background-repeat: no-repeat; background-position: center;}")
+        self.pixmap2 = QPixmap('Photos/bulb.png')
+        self.pixmap1 = QPixmap('Photos/bulbOpen.png')
+        self.label_2.setPixmap(self.pixmap2)
+        self.pixmap3 = QPixmap('Photos/nightLamp_close.png')
+        self.pixmap4 = QPixmap('Photos/nightLamp_open.png')
+        self.nightBulbLbl.setPixmap(self.pixmap3)
+        self.curtainOpenPix = QPixmap('Photos/curtainOpen.png')
+        self.curtainClosedPix = QPixmap('Photos/curtainClosed.png')
+        self.curtainLbl.setPixmap(self.curtainClosedPix)
+        self.tvOpenPix = QPixmap('Photos/uniteOpen.png')
+        self.tvClosedPix = QPixmap('Photos/uniteClosed.png')
+        self.tvLbl.setPixmap(self.tvClosedPix)
         self.artirBtn.clicked.connect(self.artirBtn_clicked)
         self.azaltBtn.clicked.connect(self.azaltBtn_clicked)
         self.count = 0
-        self.led1StatusLink = "http://192.168.137.103/5" #defining default link
-        self.led1OnLink = "http://192.168.137.103/5/on"
-        self.led1OffLink = "http://192.168.137.103/5/off"
-        self.led2StatusLink = "http://192.168.137.103/4" #defining default link
-        self.led2OnLink = "http://192.168.137.103/4/on"
-        self.led2OffLink = "http://192.168.137.103/4/off"
+        self.led1StatusLink = "http://192.168.137.103/14" #defining default link
+        self.led1OnLink = "http://192.168.137.103/14/on"
+        self.led1OffLink = "http://192.168.137.103/14/off"
+        self.led2StatusLink = "http://192.168.137.103/12" #defining default link
+        self.led2OnLink = "http://192.168.137.103/12/on"
+        self.led2OffLink = "http://192.168.137.103/12/off"
         self.curtainStatusLink = "http://192.168.137.103/2"
         self.curtainOnLink = "http://192.168.137.103/2/on"
         self.curtainOffLink = "http://192.168.137.103/2/off"
         self.led1StatusShow()
         self.led2StatusShow()
         self.curtainStatusShow()
+            
         
     @pyqtSlot()
     def artirBtn_clicked(self):
@@ -51,11 +69,10 @@ class denemem(QDialog):
         except:
             return 0
     
-    def led1StatusShow(self): #printing status of led_1
-        led1Stat = "Oda Isigi Kapali"
+    def led1StatusShow(self): #showing status of led_1
+        self.label_2.setPixmap(self.pixmap2)
         if self.led1Status():
-            led1Stat = "Oda Isigi Acik"
-        self.led1statusLbl.setText(led1Stat)
+            self.label_2.setPixmap(self.pixmap1)
 
     def led2Status(self):
         try:
@@ -65,61 +82,32 @@ class denemem(QDialog):
             return 0
 
     def led2StatusShow(self): #printing status of led_2
-        led2Stat = "Gece Lambasi Kapali"
+        self.nightBulbLbl.setPixmap(self.pixmap3)
         if self.led2Status():
-            led2Stat = "Gece Lambasi Acik"
-        self.led2statusLbl.setText(led2Stat)
+            self.nightBulbLbl.setPixmap(self.pixmap4)
 
     def curtainStatus(self):
         try:
             curtainStatus = requests.post(self.curtainStatusLink)
-            print(curtainStatus.json())
             return curtainStatus.json()
         except:
             return 0
 
     def curtainStatusShow(self): #printing status of curtain in room
-        curtainStat = "Perde Kapali"
+        self.curtainLbl.setPixmap(self.curtainClosedPix)
         if self.curtainStatus():
-            curtainStat = "Perde Acik"
-        self.curtainStatusLbl.setText(curtainStat)
+            self.curtainLbl.setPixmap(self.curtainOpenPix)
 
 
-class Book(QMainWindow):
-    def __init__(self):
-        super(Book,self).__init__()
-        loadUi('books.ui',self)
-        self.setWindowTitle('Kitap Okuma UI')
-        self.bookCount = 0
-        self.pageNo = 0
-        self.sayfaArtirBtn.clicked.connect(self.sayfaArtirBtn_clicked)
-        self.sayfaAzaltBtn.clicked.connect(self.sayfaAzaltBtn_clicked)
-        
-
-    @pyqtSlot()
-    def sayfaArtirBtn_clicked(self):
-        self.bookCount=self.bookCount + 1
-        if int(self.bookCount) > len(self.bookListWidget):
-            self.bookCount = 1
-        self.pageLbl.setText(''+str(int(self.bookCount)))
-        self.bookListWidget.setCurrentRow(self.bookCount-1)
-        print(str(self.bookListWidget.currentItem().text())) #print selected item on listview
-
-    def sayfaAzaltBtn_clicked(self):
-        self.bookCount=self.bookCount - 1
-        if int(self.bookCount) <= 0:
-            self.bookCount = len(self.bookListWidget)
-        self.pageLbl.setText(''+str(int(self.bookCount)))
-        self.bookListWidget.setCurrentRow(self.bookCount-1)
-        print(str(self.bookListWidget.currentItem().text()))
-
-bookPage=0
+bookPage = 0
+tv_frame = 0 #state of tv frame
 if __name__ == "__main__":
     #try:
     app = QtWidgets.QApplication(sys.argv)
-    widget=denemem()
+    widget=MainUI()
     widget.show()
     book = Book()
+    tv = Television()
     #sys.exit(app.exec_())
     
     while True:
@@ -137,6 +125,27 @@ if __name__ == "__main__":
                 elif book.bookCount == 3:
                     book.close()
                     bookPage = 0
+        elif tv_frame:
+            if command == "-":
+                tv.tvAzaltBtn_clicked()
+            elif command == "+":
+                tv.tvArtirBtn_clicked()
+            elif command == "c":
+                if tv.tvCount == 1:
+                    requests.post(tv.tvCh1Link)
+                    tv.tvStatusLbl.setText("1. Kanal Acik")
+                    widget.tvLbl.setPixmap(widget.tvOpenPix)
+                elif tv.tvCount == 2:
+                    requests.post(tv.tvCh2Link)
+                    tv.tvStatusLbl.setText("2. Kanal Acik")
+                    widget.tvLbl.setPixmap(widget.tvOpenPix)
+                elif tv.tvCount == 3:
+                    requests.post(tv.tvCloseLink)
+                    tv.tvStatusLbl.setText("TV Kapali")
+                    widget.tvLbl.setPixmap(widget.tvClosedPix)
+                elif tv.tvCount == 4:
+                    tv.close()
+                    tv_frame = 0
         else:
             if command == "-":
                 widget.azaltBtn_clicked()
@@ -144,8 +153,8 @@ if __name__ == "__main__":
                 widget.artirBtn_clicked()
             elif command == "c":
                 if(widget.count == 1):
-                        requests.post(widget.led1OnLink)
-                        widget.led1StatusShow()
+                    requests.post(widget.led1OnLink)
+                    widget.led1StatusShow()
                 elif(widget.count == 2):
                     requests.post(widget.led1OffLink)
                     widget.led1StatusShow()
@@ -162,6 +171,9 @@ if __name__ == "__main__":
                     requests.post(widget.curtainOffLink)
                     widget.curtainStatusShow()
                 elif(widget.count == 7):
+                    tv_frame = 1
+                    tv.show()
+                elif(widget.count == 8):
                     bookPage=1
                     book.show()
         
