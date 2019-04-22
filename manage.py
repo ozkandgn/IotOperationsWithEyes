2aa092a0f13b188be124ca7e8f6b15beaad146af
ozkandgn <ozkandgn@hotmail.com>
2019-04-22 17:47:26 +0300

    Combine and Update Interfaces

--- manage.py
+++ manage.py
@@ -5,20 +5,61 @@ import time
 
 class Manage():
     def __init__(self):
-        #self.interface_frame = Interface().get_interface_frame
+        self.interface_frame = Interface().get_interface_frame
         self.blink = Blinking().set_blink
         self.eye_frame = Eye().get_eye_frame
+        self.both_clicked = False
+        self.right_click_count = 0
+        self.left_click_count = 0
+        self.interface_frame("")
         
     def get_all_frame(self):
-        #command = input("Enter a command: ")
-        #self.interface_frame(command)
-        #time.sleep(0.5)
-        #print(self.eye_frame())
-        result = self.eye_frame()
-        if result != None:
-            print(result)
-            self.blink(result)
-        
+        result_eye = self.eye_frame()
+        if result_eye != None:
+            #for both eye
+            result = self.blink(result_eye)
+            if result == 'c' and self.both_clicked == False:
+                self.both_clicked = True
+                
+            elif result == 'c' and self.both_clicked == True:
+                result = 'n'
+                
+            elif result == 'o':
+                self.both_clicked = False
+                result = 'n'
+            
+            #for right and left eye, used counter
+            #right close
+            elif result == '+' and self.right_click_count <= 0:
+                self.right_click_count = 5
+                
+            elif result == '+' and self.right_click_count > 0:
+                self.right_click_count -= 1
+                result = 'n'
+                
+            #right open
+            elif result == 'r' and self.right_click_count > 0:
+                self.right_click_count -= 1
+                result = 'n'
+                
+            ####    
+            #left close
+            elif result == '-' and self.left_click_count <= 0:
+                self.left_click_count = 5
+                
+            elif result == '-' and self.left_click_count > 0:
+                self.left_click_count -= 1
+                result = 'n'
+                
+            #left open
+            elif result == 'l' and self.left_click_count > 0:
+                self.left_click_count -= 1
+                result = 'n'
+                
+            if result != 'n' and result != 'r' and result != 'l' and result != 'o':
+                print("result ",result)
+            self.interface_frame(result)
+
 if __name__ == "__main__":
     frames = Manage().get_all_frame
     while True:
