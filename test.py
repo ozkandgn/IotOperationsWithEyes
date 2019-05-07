57c113d886ac7aac2238ec268a4d860356054e73
ozkandgn <ozkandgn@hotmail.com>
2019-05-07 13:19:38 +0300

    Add Gamma Correction

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
-import image_filters,cv2
+from Image_Processing import image_filters
+import cv2
 
-img = cv2.imread("train3/1loo12.png",0)
+img = cv2.imread("Image_Processing/train3/1loo12.png",0)
+img = cv2.resize(img,(90,40))
+cv2.imshow("f",img)
 
-new_img = image_filters.black_and_white(img)
+light_img = image_filters.gamma_correction(img,2.7)
+
+cv2.imshow("l",light_img)
+
+new_img = image_filters.black_and_white(light_img)
+new_img2 = image_filters.black_and_white(img)
 
 cv2.imshow("i",new_img)
+cv2.imshow("i2",new_img2)
 
 cv2.waitKey(0)
